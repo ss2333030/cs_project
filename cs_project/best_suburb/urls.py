51ef9c33035490ef2e5ed21e1fe337e575987c6d
Feng Ji <fjii0003@student.monash.edu>
2022-08-13 09:28:20 +1000

    Fixed bugs in Django

--- cs_project/best_suburb/urls.py
+++ cs_project/best_suburb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name=""),
+    path("", views.index, name="index"),
     path("list", views.list, name="list"),
     path("info", views.list, name="info"),
 ]
