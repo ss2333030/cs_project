437bf8fc2799e3abc243700ee9ea25550b7235bd
zhangfeiwoaini <104955771+zhangfeiwoaini@users.noreply.github.com>
2022-09-30 10:22:27 +0800

    info

--- cs_project/best_suburb/static/best_suburb/javascript/info.js
+++ cs_project/best_suburb/static/best_suburb/javascript/info.js
@@ -18,10 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
+
 
 
 
--- cs_project/best_suburb/static/best_suburb/javascript/list.js
+++ cs_project/best_suburb/static/best_suburb/javascript/list.js
@@ -31,4 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         return false;
     });
+
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+           })
+
 });
