99b156ce22cab94601b14d09526539f2a99da690
Feng Ji <fjii2333@gmail.com>
2022-10-01 17:16:39 +1000

    Resolved conflicts

--- cs_project/best_suburb/static/best_suburb/javascript/index.js
+++ cs_project/best_suburb/static/best_suburb/javascript/index.js
@@ -1,25 +1,15 @@
-// var search = document.getElementById("search")
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#the_form").onclick = () => {
+    localStorage.setItem("uni_id", document.querySelector("#uni_id").value);
+    localStorage.setItem("rent_min", document.querySelector("#rent_min").value);
+    localStorage.setItem("rent_max", document.querySelector("#rent_max").value);
+    localStorage.setItem("crime_rate_max", document.querySelector("#crime_rate_max").value);
+    localStorage.setItem("distance_min", document.querySelector("#distance_min").value);
+    localStorage.setItem("distance_max", document.querySelector("#distance_max").value);
+  };
 
-// search.addEventListener("click", function (e) {
-//     window.location.href = "listpage.html"
-// })
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("#advanced-search").onclick = function () {
-        document.querySelector("#uni_name_advanced").value = document.querySelector("#uni_name").value;
-    };
-
-
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
 });
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});
-
-document.addEventListener("DOMContentLoaded", () =>{
-            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-           });
-        });
